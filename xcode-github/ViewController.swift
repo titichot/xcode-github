983c76562e970112264ddc356e20916231ab3ad9
titichot <projectapp302@gmail.com>
2020-04-02 13:18:18 +0700

    Initial Commit

---
+++ xcode-github/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xcode-github
+//
+//  Created by FRANK on 2/4/2563 BE.
+//  Copyright © 2563 mindfrank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
